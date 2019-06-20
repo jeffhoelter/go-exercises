588f599da3228e96ceb27d1c033ee3e1ae531fde
jeff_hoelter <jeffh@loar.net>
2019-06-19 22:39:14 -0400

    Level 1, exercises 1 and 2.

---
+++ level1-2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Printf("x : %v, y : %v, z : %v\n", x, y, z)
+}
